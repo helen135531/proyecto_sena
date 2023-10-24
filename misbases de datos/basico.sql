c50488d93001bd45a9d45d72e28e920a61b8af7c
helen135531 <helentatiana170707@gmail.com>
2023-10-24 13:44:43 -0500

    hola :D

--- misbases de datos/basico.sql
+++ misbases de datos/basico.sql
@@ -2,7 +2,9 @@ use proyecto_sena;
 show tables;
 select * from usuario;
 select * from producto;
+select * from pedido;
 
+insert into pedido (fechaPedido,horaPedido,totalPedido,estadoPedido,pedidoaDomicilio,idUSuarioFK) values ('24/10/23',0131,40000,'en camino','si',3);
  insert into Usuario (tipoDocUsuario,noDocUsuario,nombreUsuario,apellidoUsuario,direccionUsuario,telefonoUsuario,correoUsuario,passwordusuario,estadoUsuario,idRolUsuarioFK) value 
  ('cedula',7854437,'helen','hernandez','Calle 10 No. 9 - 78  Centro ',3245623754,'helentatiana170707@gmail.com','moreno123' ,'inactivo',3);
 
